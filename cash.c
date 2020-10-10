b0c4b366500b4093b51907ebe1762bf327b884ab
t-uddin <t-uddin@users.noreply.github.com>
2020-10-10 14:43:53 +0000

    automated commit by check50 [check50=True]

--- cash.c
+++ cash.c
@@ -4,8 +4,8 @@
 
 int main(void)
 {
-  //Prompt user for change amount
-  float change;
+    //Prompt user for change amount
+    float change;
     do
     {
         change = get_float("Change owed: ");
@@ -15,7 +15,7 @@ int main(void)
     //Convert dollars to cents and convert to integer to prevent floating point imprecision
     int cents = round(change * 100);
 
-    //Print minimum number of coins to return
+    //Calculate minimum number of coins to return
     int coins = 0;
 
     for (int quarters = 0; cents >= 25; coins++)
@@ -23,20 +23,21 @@ int main(void)
         cents = cents - 25;
     }
 
-    for (int dimes = 0; cents >=10; coins++)
+    for (int dimes = 0; cents >= 10; coins++)
     {
         cents = cents - 10;
     }
 
-    for (int nickels = 0; cents>=5; coins++)
+    for (int nickels = 0; cents >= 5; coins++)
     {
         cents = cents - 5;
     }
 
-    for (int dimes = 0; cents>=1; coins++)
+    for (int dimes = 0; cents >= 1; coins++)
     {
         cents = cents - 1;
     }
 
+    //Print number of coins to return
     printf("%i\n", coins);
 }
